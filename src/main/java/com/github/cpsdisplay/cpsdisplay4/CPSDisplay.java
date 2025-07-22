2f8ff5f29d8d107b7258638a7967f4cad60cf0fb
Dams4K <dams4k@users.noreply.github.com>
2025-07-22 10:29:54 +0200

    setup

--- src/main/java/com/github/cpsdisplay/cpsdisplay4/CPSDisplay.java
+++ src/main/java/com/github/cpsdisplay/cpsdisplay4/CPSDisplay.java
@@ -6,7 +6,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 @Mod(modid = "cpsdisplay", useMetadata=true)
-public class ExampleMod {
+public class CPSDisplay {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
         System.out.println("Dirt: " + Blocks.dirt.getUnlocalizedName());
