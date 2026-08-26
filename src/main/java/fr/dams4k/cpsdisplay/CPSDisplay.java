8580f298cf940e0449f4d56a6ac5852d0a753d48
Dams4K <dams4k@users.noreply.github.com>
2026-08-26 15:13:42 +0200

    bckp

--- src/main/java/fr/dams4k/cpsdisplay/CPSDisplay.java
+++ src/main/java/fr/dams4k/cpsdisplay/CPSDisplay.java
@@ -1,20 +1,35 @@
 package fr.dams4k.cpsdisplay;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import fr.dams4k.cpscore.CPSCore;
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.init.Blocks;
+import fr.dams4k.cpscore.Component;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.GameSettings;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
 
 @Mod(modid = "cpsdisplay", useMetadata=true)
 public class CPSDisplay {
     @Mod.EventHandler
-    public void init(FMLInitializationEvent event) {
-        System.out.println("Dirt: " + Blocks.dirt.getUnlocalizedName());
-		// Below is a demonstration of an access-transformed class access.
-        System.out.println("Color State: " + new GlStateManager.Color());
+    public void init(FMLInitializationEvent event) throws IOException {
+        CPSCore.init(getModFolder());
 
-        CPSCore core = new CPSCore();
-        core.testGson();
+        GameSettings gs = Minecraft.getMinecraft().gameSettings;
+//        gs.
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        Component component = new Component();
+        component.text = "[{0} | {1}] CPS";
+        gson.toJson(component, new FileWriter(getModFolder().resolve("0.json").toString()));
+    }
+
+    public static Path getModFolder() {
+        return Loader.instance().getConfigDir().toPath().resolve("cpsdisplay");
     }
 }
